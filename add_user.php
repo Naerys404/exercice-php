901b641776f1bc97dea0a2e87560d9849af9ac50
LauraG <naerys.404@gmail.com>
2026-01-16 09:07:19 +0100

    fix roles

--- add_user.php
+++ add_user.php
@@ -5,12 +5,13 @@ include 'tools.php';
 if (isset($_POST["submit"])) {
 
     if (
-        !empty($_POST["firstname"]) && 
-        !empty($_POST["lastname"]) && 
+        !empty($_POST["firstname"]) &&
+        !empty($_POST["lastname"]) &&
         !empty($_POST["email"]) &&
-        !empty($_POST["password"])) {
+        !empty($_POST["password"])
+    ) {
         sanitize_array($_POST);
-   
+
         $_POST["password"] = password_hash($_POST["password"], PASSWORD_DEFAULT);
 
         $message = add_user($_POST);
@@ -23,22 +24,25 @@ if (isset($_POST["submit"])) {
 function add_user(array $user)
 {
     $role = "utilisateur";
-    try  {
-    
+    try {
+
         $bdd = connect_bdd();
-      
+
         $sql = "INSERT INTO users(firstname, lastname, email,`password`, roles) VALUE(?,?,?,?,?)";
- 
+
         $req = $bdd->prepare($sql);
 
         $req->bindValue(1, $user["firstname"], PDO::PARAM_STR);
         $req->bindValue(2, $user["lastname"], PDO::PARAM_STR);
         $req->bindValue(3, $user["email"], PDO::PARAM_STR);
         $req->bindValue(4, $user["password"], PDO::PARAM_STR);
-        $req->bindValue(5, $role, PDO::PARAM_STR).
+        if (isset($user['roles']) && $user['roles'] === 'on') {
+            $req->bindValue(5, 'ROLE_ADMIN', PDO::PARAM_STR);
+        } else {
+            $req->bindValue(5, 'ROLE_USER', PDO::PARAM_STR);
+        }
         $req->execute();
-    } catch(Exception $e)
-    {
+    } catch (Exception $e) {
         return "Le compte existe déja";
     }
     return "Le compte à été ajouté en BDD";
@@ -47,6 +51,7 @@ function add_user(array $user)
 
 
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -55,8 +60,9 @@ function add_user(array $user)
         href="https://cdn.jsdelivr.net/npm/@picocss/pico@2/css/pico.min.css">
     <title>Ajouter un compte</title>
 </head>
+
 <body>
-    
+
     <main class="container">
         <h1>Ajouter un compte</h1>
         <form action="" method="post">
@@ -65,10 +71,15 @@ function add_user(array $user)
                 <input type="text" name="lastname" placeholder="Saisir votre nom">
                 <input type="email" name="email" placeholder="Saisir votre email">
                 <input type="password" name="password" placeholder="Saisir votre mot de passe">
+                <label for="roles">
+                    Cocher pour rôle admin
+                    <input type="checkbox" id="roles" name="roles" aria-label="Rôle administrateur">
+                </label>
             </fieldset>
             <input type="submit" value="Ajouter" name="submit">
         </form>
-        <p><?= $message ?? ""?></p>
+        <p><?= $message ?? "" ?></p>
     </main>
 </body>
+
 </html>
